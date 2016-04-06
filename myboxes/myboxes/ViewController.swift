361f3d5aafb15b07bd521e4bc41a72107d165956
Jeffery Liu <jefferyandjin@gmail.com>
2016-04-06 15:18:41 -0600

    Empty Project

---
+++ myboxes/myboxes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myboxes
+//
+//  Created by Jeffery Liu on 2016-04-06.
+//  Copyright © 2016 daybreakjj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
